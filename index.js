eb937115b5428c6a1e2e81ff04ef6416c0be2243
Igor <igor.s@xola.com>
2022-10-03 23:21:51 +0200

    Fix

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-module.exports = {
-    arrayToString(a) {
-        return a.join(" ");
-    },
+function arrayToString(a) {
+    return a.join(" ");
+}
 
-    arrayLog(a) {
-        process.stdout.write(arrayToString(a));
-        process.stdout.write("\n");
-    },
+function arrayLog(a) {
+    process.stdout.write(arrayToString(a));
+    process.stdout.write("\n");
+}
 
-    intToBin(n, p = 8) {
-        if (n < 0) {
-            n = Math.pow(2, p) + n;
-        }
-
-        return Math.abs(n).toString(2).padStart(p, "0");
-    },
-
-    binLog(n, p = 8) {
-        process.stdout.write(intToBin(n, p));
-        process.stdout.write("\n");
+function intToBin(n, p = 8) {
+    if (n < 0) {
+        n = Math.pow(2, p) + n;
     }
+
+    return Math.abs(n).toString(2).padStart(p, "0");
+}
+
+function binLog(n, p = 8) {
+    process.stdout.write(intToBin(n, p));
+    process.stdout.write("\n");
+}
+
+module.exports = {
+    arrayToString,
+    arrayLog,
+    intToBin,
+    binLog
 };
 
